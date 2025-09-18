9d49aecaebf46b4d9fa0e7bb863b648e60bbbba7
Trung <trungnnde180575@fpt.edu.vn>
2025-09-18 21:30:28 +0700

    Update MovieCate.js

--- src/component/MovieCate.js
+++ src/component/MovieCate.js
@@ -1,29 +1,27 @@
-//done
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useLocation, useParams } from 'react-router-dom';
-import { fetchMovieCate, fetchMovieDetails } from '../service/MovieService';
-import { setCategoryMovies, setMovieDetails } from './Slice/CategoryMoviesSlice';
+import { fetchMovieCate } from '../service/MovieService';
+import { setCategoryMovies } from './Slice/CategoryMoviesSlice';
 import '../assest/MovieCate.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlayCircle } from '@fortawesome/free-regular-svg-icons';
 import LoadingComponent from './LoadingComponent';
-import Pagination from './Pagination'; // Import the Pagination component
+import Pagination from './Pagination';
 import { Helmet } from 'react-helmet';
 
 function MovieCate() {
-
     const { cate } = useParams();
     const location = useLocation();
     const dispatch = useDispatch();
-    const [currentPage, setCurrentPage] = useState(1);
     const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        const query = new URLSearchParams(location.search);
-        const page = query.get('page') || 1;
-        setCurrentPage(Number(page));
-    }, [location.search]);
+
+    // Đọc currentPage trực tiếp từ URL
+    const query = new URLSearchParams(location.search);
+    const currentPage = Number(query.get('page')) || 1;
+
     const categoryMovies = useSelector((state) => state.categoryMovies);
+
     useEffect(() => {
         const loadMovieCate = async () => {
             setLoading(true);
@@ -32,11 +30,10 @@ function MovieCate() {
             setLoading(false);
         };
         loadMovieCate();
-    }, [dispatch, cate, currentPage]);
+    }, [dispatch, cate, currentPage, location.search]);
 
-    //const categoryMovies = useSelector((state) => state.categoryMovies);
     if (loading) {
-        return <LoadingComponent />; // Render LoadingComponent when loading
+        return <LoadingComponent />;
     }
     if (!categoryMovies.cat) {
         return <LoadingComponent />;
@@ -50,14 +47,13 @@ function MovieCate() {
             <h4>Danh sách {categoryMovies.cat.name.includes('Phim') ? categoryMovies.cat.name : 'Phim ' + categoryMovies.cat.name}</h4>
             <div className='row'>
                 {categoryMovies.items.map((movie) => (
-                    <div className='col-6 col-md-3 col-sm-4 col-xl-2 mb-3'>
+                    <div className='col-6 col-md-3 col-sm-4 col-xl-2 mb-3' key={movie._id || movie.slug}>
                         <div className='card position-relative tooltip-wrapper border-0 w-100 movie--width1'>
                             <div className='img-container-cate position-relative overflow-hidden'>
                                 <img
                                     src={movie.thumb_url}
                                     alt={movie.name}
                                     className='hover-thumb w-100'
-                                    // style={{height:'350px'}}
                                 />
                                 <div className="play-button">
                                     <Link to={`/watch/${movie.slug}`}>
